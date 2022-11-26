81545c7fd77dcb64d581cf2050eb962453a5a4dc
akash maurya <115409020+zeoon-cooder@users.noreply.github.com>
2022-11-26 16:13:28 +0530

    Add files via upload

---
+++ PYTHON PROJECT 14.py
@@ -0,0 +1,59 @@
+print(''' PYTHON PROJECT 14
+
+             by:
+
+                Priyanshu Raj (Roll number:-43)
+
+                Abhishek kr. Singh(Roll num.:-44)
+
+                Akash Maurya (Roll num.:-45)
+
+''')
+
+n = input("Enter name of student(in lowercase only):-")
+
+
+
+project={
+
+    "mantu kumar":{"contact":6202283515},
+
+    "abhishek kumar":{"contact":6204883887},
+
+    "vivek kumar maurya":{"contact":6306934472},
+
+    "zaid kazmi":{"contact":6353702759},
+
+    "kartheek":{"contact":7013544881},
+
+    "koventhan m":{"contact":7339698656},
+
+    "sridhar patel":{"contact":9857358587},
+
+    "maroof":{"contact":6495730583},
+
+    "karthi sai":{"contact":7594730672},
+
+    "lalit sharma":{"contact":5869473967},
+
+    "azad rajput":{"contact":9674659730},
+
+    "saksham srivastava":{"contact":9569463865},
+
+    "mukul yadav":{"contact":9674659702},
+
+    "gopal":{"contact":6804769429},
+
+    "aditya kumar":{"contact":9647603647},
+
+    "raghav deedwaniya":{"contact":6743967499},
+
+    "shivam thakur":{"contact":68499673086},
+
+    "abhishek kumar singh":{"contact":6206500860},
+
+    "priyanshu raj":{"contact":6684650375},
+
+    }
+
+print("the contact no is.",project[n]["contact"])
